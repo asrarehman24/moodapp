4d1cd4127c6146f2a7da848247a9da4325007abd
Asra <2022ce23@student.uet.edu.pk>
2025-08-16 18:00:03 +0500

    step-6

--- lib/views/login_view.dart
+++ lib/views/login_view.dart
@@ -1,9 +1,8 @@
 //import 'dart:developer';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'dart:developer' as devtools show log;
-
 import 'package:mynotes/constants/routes.dart';
+import 'package:mynotes/utilities/show_error_dialog.dart';
 
 class LoginView extends StatefulWidget {
   const LoginView({super.key});
@@ -67,11 +66,15 @@ class _LoginViewState extends State<LoginView> {
                 ).pushNamedAndRemoveUntil(notesRoutes, (route) => false);
               } on FirebaseAuthException catch (e) {
                 if (e.code == 'invalid-credential') {
-                  devtools.log('Invalid email or password. Please try again.');
+                  await showErrorDialog(
+                    context,
+                    'Invalid email or password. Please try again.',
+                  );
                 } else {
-                  devtools.log('Something else happened');
-                  devtools.log(e.code.toString());
+                  await showErrorDialog(context, 'Error: ${e.code}');
                 }
+              } catch (e) {
+                await showErrorDialog(context, e.toString());
               }
             },
             child: const Text('Login'),
